d05e0e115c558184effeb71ac57adc94c4aee766
Christopher Zientek <christopherzientek@Christophers-MacBook-Pro.local>
2020-06-19 20:00:22 -0400

    simplified form to just company name

--- pages/tea.vue
+++ pages/tea.vue
@@ -63,34 +63,6 @@ export default {
     teaNames() {
       return Object.keys(this.teaTypes);
     }
-  },
-  methods: {
-    // This function puts all the form fields into a FormData constructor, which we later encode with the URLSearchParams constructor
-    createFormDataObj(data) {
-      const formData = new FormData();
-      for (const key of Object.keys(data)) {
-        formData.append(key, data[key]);
-      }
-      return formData;
-    },
-    // This is our custom onSubmit function; don't forget to add `@submit.prevent="handleSubmit"` inside your <form> tag
-    handleSubmit() {
-      // This `data` object is what's passed to the createFormDataObj function. It needs all of your form fields, where the key is the name= attribute and the value is the computed value.
-      const data = {
-        "form-name": "vue-tea",
-        tea: this.teaName
-      };
-      // This POSTs your encoded form to Netlify with the required headers (for text; headers will be different for POSTing a file) and, on success, redirects to the custom success page located at pages/thanks.vue
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(this.createFormDataObj(data)).toString()
-      })
-        // This is how we route to /thanks on successful form submission
-        // More on $router.push function: https://router.vuejs.org/guide/essentials/navigation.html
-        .then(() => this.$router.push("/"))
-        .catch(error => alert(error));
-    }
   }
 };
 </script>
