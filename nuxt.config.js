79562067333b8afa9cec73530dd9cd52079773ba
Christopher Zientek <christopherzientek@Christophers-MacBook-Pro.local>
2020-06-22 17:00:32 -0400

    updated plugin for modal

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   */
  css: [{ src: '~/assets/main.scss', lang: 'scss' }],
 
- 
+
  fontawesome: {
   imports: [
     {
@@ -40,7 +40,7 @@ export default {
   ** Plugins to load before mounting the App
   */
  plugins: [
-  {src: '~plugins/vue-js-modal.js', mode: 'client'}
+  '~plugins/vue-js-modal.js'
 ],
   /*
   ** Nuxt.js dev-modules
