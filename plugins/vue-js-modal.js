0ef4d5f84a8d5496d28c95c21e549c7ca2a69edd
Christopher Zientek <christopherzientek@Christophers-MacBook-Pro.local>
2020-06-22 17:56:11 -0400

    oh well

--- plugins/vue-js-modal.js
+++ plugins/vue-js-modal.js
@@ -5,10 +5,10 @@ import VModal from 'vue-js-modal'
 import 'vue-js-modal/dist/styles.css';
 
 Vue.use(VModal, {
-	dialog: true,
-	dynamic: true,
-	injectModalsContainer: true,
-	dynamicDefaults: {
-		foo: 'foo'
-	}
+	// dialog: true,
+	// dynamic: true,
+	// injectModalsContainer: true,
+	// dynamicDefaults: {
+	// 	foo: 'foo'
+	// }
 })
